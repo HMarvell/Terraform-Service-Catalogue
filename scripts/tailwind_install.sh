3d8a7cba7153d2c4f6a4d33e8014cd948339ade3
Harry Marvell <113520169+HMarvell@users.noreply.github.com>
2025-04-24 20:06:27 +0100

    Update tailwind_install.sh

--- scripts/tailwind_install.sh
+++ scripts/tailwind_install.sh
@@ -1,15 +1,15 @@
 #! bin/bash
-### This is a script that will allow for package management to be pulled as a module, reducing the size of the main .sh.
+### This is a script that will allow for tailwind config to be pulled as a module, reducing the size of the main .sh.
 ### This file should be installed in the following location on the EC2 instance "/tmp/react-files"
-
-### Package management###
-sudo yum update -y
-# Pull and install node version manager (nvm)
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
-# Load nvm
-source ~/.bashrc
-# Use nvm to install node18 supported on AL2
-nvm install 18
-# Install Nginx
-sudo amazon-linux-extras enable nginx1
-sudo yum install nginx -y
+cat <<EOF > tailwind.config.js
+module.exports = {
+    content: ["./index.html", "./src/**/*{js,ts,jsx,tsx}"],
+    theme: { extends {} },
+    plugins: [],
+}
+EOF
+cat << EOF > src/index.css
+@tailwindbase;
+@tailwind components;
+@tailwind utilities;
+EOF
